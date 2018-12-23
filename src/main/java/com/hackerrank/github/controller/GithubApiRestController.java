d30ce1b16d242c4287ba3676837d178eaf2a7e5c
HackerRank <hackerrank@example.com>
2018-12-23 23:12:04 +0000

    Saved at Sun Dec 23 2018 22:12:02 GMT-0100 (Cape Verde Standard Time)

--- src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
+++ src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
@@ -45,7 +45,7 @@ public class GithubApiRestController {
     @DeleteMapping(value = "/erase")
     public ResponseEntity deleteEvents() {
         eventRepository.deleteAll();
-
+        actor
         return ResponseEntity.ok().build();
     }
 
