9aa9725672508c1f31b0f011eb15cc728c59b7d9
Randy Hector Bartumeu Huergo <randy@aleph.engineering>
2018-12-23 21:47:46 -0100

    Add Delete events endpoint

--- src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
+++ src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
@@ -4,4 +4,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GithubApiRestController {
-}
+
+    private final EventRepository eventRepository;
+    private final ActorRepository actorRepository;
+    private final RepoRepository repoRepository;
+
+    public GithubApiRestController(EventRepository eventRepository, ActorRepository actorRepository, RepoRepository repoRepository) {
+        this.eventRepository = eventRepository;
+        this.actorRepository = actorRepository;
+        this.repoRepository = repoRepository;
+    }
+
+    @DeleteMapping(value = "/erase")
+    public ResponseEntity deleteEvents() {
+        eventRepository.deleteAll();
+        return ResponseEntity.ok().build();
+    }
