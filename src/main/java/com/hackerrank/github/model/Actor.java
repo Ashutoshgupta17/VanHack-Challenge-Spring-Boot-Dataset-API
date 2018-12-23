f9821f33242582d6e282a4de7387373d61f9c919
Randy Hector Bartumeu Huergo <randy@aleph.engineering>
2018-12-23 21:39:27 -0100

    Complete model classes utilizing lombok anotations

--- src/main/java/com/hackerrank/github/model/Actor.java
+++ src/main/java/com/hackerrank/github/model/Actor.java
@@ -1,40 +1,20 @@
 package com.hackerrank.github.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Actor {
+
+    @Id
     private Long id;
     private String login;
     private String avatar;
-
-    public Actor() {
-    }
-
-    public Actor(Long id, String login, String avatar) {
-        this.id = id;
-        this.login = login;
-        this.avatar = avatar;
-    }
-    
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public String getLogin() {
-        return login;
-    }
-    
-    public void setLogin(String login) {
-        this.login = login;
-    }
-    
-    public String getAvatar() {
-        return avatar;
-    }
-    
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
 }
--- src/main/java/com/hackerrank/github/model/Event.java
+++ src/main/java/com/hackerrank/github/model/Event.java
@@ -1,62 +1,27 @@
 package com.hackerrank.github.model;
 
+import lombok.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+
+    @Id
     private Long id;
     private String type;
+    @OneToOne(cascade = CascadeType.ALL)
     private Actor actor;
+    @OneToOne(cascade = CascadeType.ALL)
     private Repo repo;
     private Timestamp createdAt;
-
-    public Event() {
-    }
-
-    public Event(Long id, String type, Actor actor, Repo repo, Timestamp createdAt) {
-        this.id = id;
-        this.type = type;
-        this.actor = actor;
-        this.repo = repo;
-        this.createdAt = createdAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Actor getActor() {
-        return actor;
-    }
-
-    public void setActor(Actor actor) {
-        this.actor = actor;
-    }
-
-    public Repo getRepo() {
-        return repo;
-    }
-
-    public void setRepo(Repo repo) {
-        this.repo = repo;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
 }
